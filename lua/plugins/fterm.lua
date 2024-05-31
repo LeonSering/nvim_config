656ffb2dad3b82fe570be2d7cc3bd873cc88789e
Leon Sering <leon@sering.eu>
2024-05-31 23:19:55 +0200

    added fterm for floating terminal, cargo commands run in vertical split

---
+++ lua/plugins/fterm.lua
@@ -0,0 +1,17 @@
+return {
+  'numToStr/FTerm.nvim',
+  config = function()
+    require 'FTerm'.setup({
+      border     = 'single',
+      dimensions = {
+        height = 0.8,
+        width = 0.5,
+      },
+    })
+
+    -- Example keybindings
+    vim.keymap.set({'n', 'i'}, '<C-\\>', '<CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set('t', '<C-\\>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+    vim.keymap.set('t', '<esc>', '<C-\\><C-n><CMD>lua require("FTerm").close()<CR>')
+  end
+}
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -4,15 +4,14 @@ return {
   config = function()
     -- run RustFmt on the current file only
     -- cargo test and scroll to the bottom of the vim-terminal
-    vim.keymap.set('n', '<leader>ct', ':RustTest<CR>G', { desc = "Cargo: Run current test" })   -- run test under cursor
-    vim.keymap.set('n', '<leader>cT', ':RustTest!<CR>G', { desc = "Cargo: Run all tests" })     -- run all tests
-    vim.keymap.set('n', '<leader>CT', ':RustTest!<CR>G', { desc = "Cargo: Run all tests" }) -- run all tests
-
-    -- cargo run and scroll to the bottom of the vim-terminal
+    vim.keymap.set('n', '<leader>ct', ':vertical RustTest<CR>G', { desc = "Cargo: Run current test" }) -- run test under cursor
+    vim.keymap.set('n', '<leader>cT', ':vertical RustTest!<CR>G', { desc = "Cargo: Run all tests" })   -- run all tests
+    vim.keymap.set('n', '<leader>CT', ':vertical RustTest!<CR>G', { desc = "Cargo: Run all tests" })   -- run all tests
     vim.keymap.set('n', '<leader>cr', ':Crun<CR>G', { desc = "Cargo: Run" })
-    -- cargo build and scroll to the bottom of the vim-terminal
     vim.keymap.set('n', '<leader>cb', ':Cbuild<CR>G', { desc = "Cargo: Build" })
-    vim.g.rustfmt_autosave = 1 -- automatic run :RustFmt on save
+    vim.keymap.set('n', '<leader>cc', ':Cargo clippy<CR>G', { desc = "Cargo: Clippy" })
+    vim.g.cargo_shell_command_runner = 'vsplit | terminal' -- open terminal in a vertical split
 
+    vim.g.rustfmt_autosave = 1 -- automatic run :RustFmt on save
   end
 }
