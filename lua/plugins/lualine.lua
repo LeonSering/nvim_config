4d5ee078f99ed267ef7ba4324f3fea06a6cfd9be
Leon Sering <leon.sering@moia.io>
2025-09-03 23:37:58 +0200

    new look for the lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,33 +3,51 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
-  opts = {
-    -- options = {theme = 'papercolor_dark'},
-    sections = {
-      lualine_a = { 'mode' },
-      lualine_b = { 'branch', 'diff', 'diagnostics' },
-      lualine_c = {
-        { 'filename', path = 1, color = { fg = 'lightgrey' } },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'auto',
       },
-      lualine_x = {
-        {
-          function()
-            local space = vim.fn.search([[\s\+$]], 'nwc')
-            return space ~= 0 and "TW:" .. space or ""
-          end,
-          color = { fg = 'red' }
+      extensions = { 'nvim-dap-ui', 'aerial' },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = {
+          { 'filename', path = 3, color = { fg = 'white' } },
         },
-        'encoding',
-        'filetype'
+        lualine_c = {
+          {
+            'diagnostics',
+            sections = { 'error', 'warn' },
+
+          },
+          {
+            function()
+              local space = vim.fn.search([[\s\+$]], 'nwc')
+              return space ~= 0 and "TW:" .. space or ""
+            end,
+            color = { fg = 'red' }
+          }
+        },
+        lualine_x = {
+          'diff',
+        },
+        lualine_y = {
+          -- 'filetype',
+          { 'branch', color = { fg = 'lightgrey' } },
+          'encoding',
+        },
+        lualine_z = { 'location' },
       },
-      lualine_y = { 'progress' },
-      lualine_z = { 'location' },
-    },
-    inactive_sections = {
-      lualine_c = {
-        { 'filename', path = 1, color = { fg = 'lightgrey' } },
+      inactive_sections = {
+        lualine_b = {
+          -- { 'filename', path = 1, color = { fg = 'lightgrey' } },
+        },
+        lualine_c = {},
+        lualine_x = {},
       },
-      lualine_x = {},
-    },
-  }
+    })
+    vim.api.nvim_set_hl(0, 'LuaLineDiffAdd', { ctermfg = 28, fg = 'Green4', bold = true })
+    vim.api.nvim_set_hl(0, 'LuaLineDiffChange', { ctermfg = 25, fg = 'DeepSkyBlue4', bold = true })
+    vim.api.nvim_set_hl(0, 'LuaLineDiffDelete', { ctermfg = 124, fg = 'Red3', bold = true })
+  end
 }
